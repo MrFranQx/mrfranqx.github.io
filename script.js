d9e67b7567f9c3bb5eb4d0d95f05d05f8559d67d
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2024-09-09 22:47:20 +0200

    Finish navigation; refactor; update copyright

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function footerMargin() {
-    let f = document.getElementsByTagName("footer")[0];
-    f.style.marginBottom = window.getComputedStyle(f).marginLeft;
-
+    const f = document.querySelector("body > footer");
+    if (media.matches) {
+        f.style.marginBottom = window.getComputedStyle(f).marginLeft;
+    }
+    else {
+        f.style.marginBottom = 0;
+    }
 }
 function onScroll() {
-    let scroll = window.scrollY;
-    let button = document.getElementById("scrollup");
+    const scroll = window.scrollY;
+    const button = document.querySelector("#scrollup");
     if (scroll > 100)
         button.style.opacity = 1;
     else
@@ -15,11 +19,29 @@ function scrollToTop() {
     window.scrollTo(0,0);
 }
 function setSublistWidth() {
-    let menus = document.querySelector("nav").querySelector("ul").querySelectorAll("ul");
-    for (let menu of menus) {
-        menu.style.width = window.getComputedStyle(menu.parentElement).width;
-    }
+    const menus = document.querySelectorAll("body > nav > ul > li > ul");
+        for (const menu of menus) {
+            if (media.matches) {
+                menu.style.width = window.getComputedStyle(menu.parentElement).width;
+            }
+            else {
+                menu.style.width = "";
+            }
+        }
 }
+function showNavi() {
+    document.querySelector("body > nav").classList.toggle("active");
+    document.querySelector("div.hide").classList.toggle("active");
+}
+
+function toggleMenu() {
+    this.parentElement.classList.toggle("active");
+    const i = this.querySelector("i");
+    i.classList.toggle("fa-chevron-up");
+    i.classList.toggle("fa-chevron-down");
+}
+
+let media = window.matchMedia("screen and (width >= 768px)");
 
 footerMargin();
 setSublistWidth();
@@ -27,4 +49,12 @@ setSublistWidth();
 window.addEventListener("resize",footerMargin);
 window.addEventListener("resize",setSublistWidth);
 window.addEventListener("scroll",onScroll);
-document.getElementById("scrollup").addEventListener("click",scrollToTop)
+
+document.querySelector("#scrollup").addEventListener("click",scrollToTop)
+
+document.querySelector("#navi").addEventListener("click", showNavi);
+document.querySelector("div.hide").addEventListener("click", showNavi);
+
+document.querySelectorAll("body > nav > ul > li > button").forEach(
+    elem => elem.addEventListener("click",toggleMenu)
+);
